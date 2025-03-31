4e18d9fe1522edaee6e926c1dc73c67c21466b55
julianhernao.marin.contractor.gdevtools.com <julianhernao.marin.contractor@gdevtools.com>
2025-03-31 14:52:48 -0500

    Initial code

---
+++ src/main/java/com/example/spring_boot_caffeine_cache/SpringBootCaffeineCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_caffeine_cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCaffeineCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCaffeineCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_caffeine_cache/SpringBootCaffeineCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_caffeine_cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCaffeineCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
